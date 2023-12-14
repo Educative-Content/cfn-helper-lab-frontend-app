f35787aac632c2d7ee31be628c125e35d38a2f2a
Arham-Educative <133845255+Arham-Educative@users.noreply.github.com>
2023-12-14 13:18:36 +0500

    Update Loading.jsx

--- src/components/loading/Loading.jsx
+++ src/components/loading/Loading.jsx
@@ -19,6 +19,15 @@ function Loading() {
     <Container>
       <Col className="align-items-center">
         <Row style={{ paddingTop: '10px' }}>
+          <h1 className="header1-design">Add To-Do Item</h1>
+        </Row>
+        <Row style={{ paddingTop: '20px', paddingBottom: '20px' }}>
+          <CardList data={{todoList: [{ id: 99 }]}} type="loader" />
+        </Row>
+        <Row style={{ paddingTop: '20px' }}>
+          <hr style={{ color: '#ffffff' }} />
+        </Row>
+        <Row style={{ paddingTop: '10px' }}>
           <h1 className="header1-design">To-Do List</h1>
         </Row>
         <Row style={{ paddingTop: '20px', paddingBottom: '20px' }}>
